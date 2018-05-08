ede054a22c172fea8da145d726d048ca5e2793dd
Stanley Muzhuthettu <smuzh001@sledge.cs.ucr.edu>
2018-05-08 13:16:23 -0700

    added priority to scheduling

--- proc.c
+++ proc.c
@@ -358,6 +358,9 @@ waitpid(int pid, int *status, int options){
 	release(&ptable.lock);
 	return -1;
     }
+	if(curproc->priority > 0){//decrementing priority
+       		curproc->priority = curproc->priority - 1;
+	}
     sleep(curproc, &ptable.lock);
   }
 
@@ -379,37 +382,53 @@ scheduler(void)
   struct proc *p;
   struct cpu *c = mycpu();
   c->proc = 0;
+  struct proc *temp = 0;  
+  //for(temp = ptable.proc; temp < &ptable.proc[NPROC]; temp++){
+	
+  
+  
   
   for(;;){
+    int minPriority = 32;
     // Enable interrupts on this processor.
     sti();
+   
+    //
+
 
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE)
         continue;
+      if (p->priority < minPriority) {
+	minPriority = p->priority;
+	temp = p;
+      }
+    }
+    // Switch to chosen process.  It is the process's job
+    // to release ptable.lock and then reacquire it
+    // before jumping back to us.
+   if(temp){ 
+   c->proc = temp;
+    switchuvm(temp);
+    temp->state = RUNNING;
+    if(temp->priority < 31)
+    	temp->priority = temp->priority + 1;
+    
+    swtch(&(c->scheduler), temp->context);
+    switchkvm();
 
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
-
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
+    // Process is done running for now.
+    // It should have changed its p->state before coming back.
+    c->proc = 0;
     }
     release(&ptable.lock);
-
+	
   }
 }
 
-void setpriority(int priority) {
+void setpriority(int priority) {	//added for lab2test
 
 }
 
